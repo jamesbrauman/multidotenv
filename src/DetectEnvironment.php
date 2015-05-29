ea5e76b1b6fb71223271ea10c2c265c928ab1e9d
thesnackalicious <james.22au@gmail.com>
2015-05-29 13:10:47 +0000

    Adding DetectEnvironment.php to repository.

---
+++ src/DetectEnvironment.php
@@ -0,0 +1,24 @@
+<?php namespace MultiDotEnv;
+
+use Dotenv;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\DetectEnvironment as BaseDetectEnvironment;
+
+class DetectEnvironment extends BaseDetectEnvironment {
+
+    /**
+     * Bootstrap the given application.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function bootstrap(Application $app)
+    {
+        parent::bootstrap($app);
+
+        $envFile = '.env.' . $app->environment();
+
+        if (file_exists($app->basePath() . DIRECTORY_SEPARATOR . $envFile))
+            Dotenv::load($app->basePath(), $envFile);
+    }
+}
